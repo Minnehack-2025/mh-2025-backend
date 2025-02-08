ce5b17951c4d4694490c6909dfb332d9976ddbdc
Caden Jurkovski <jurko036@umn.edu>
2025-02-08 17:49:57 -0600

    added db configuration

--- api/__init__.py
+++ api/__init__.py
@@ -1,11 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
 
 def create_app():
     app = Flask(__name__)
 
+    load_dotenv()
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_pre_ping': True}
+
     with app.app_context():
         from .routes import init_routes
+        from .models import init_db
 
         init_routes(app)
+        init_db(app)
 
     return app
