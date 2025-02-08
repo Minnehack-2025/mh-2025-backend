f2c7a980da1314608698ebf4a0686e53685a0edc
Caden Jurkovski <jurko036@umn.edu>
2025-02-08 15:08:33 -0600

    add gunicorn configuration

---
+++ gunicorn_config.py
@@ -0,0 +1,11 @@
+import logging
+import multiprocessing
+
+workers = multiprocessing.cpu_count() * 2 + 1
+bind = "0.0.0.0:8000"
+loglevel = "info"
+accesslog = "-"  # Log to stdout
+errorlog = "-"   # Log to stderr
+
+# Configure Gunicorn logging
+logger = logging.getLogger('gunicorn.error')
--- wsgi.py
+++ wsgi.py
@@ -2,5 +2,6 @@ from api import create_app
 
 app = create_app()
 
+# for development
 if __name__ == "__main__":
     app.run()
